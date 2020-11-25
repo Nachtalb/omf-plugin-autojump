d9e42b0c15a7ff360a6defa79fbdafb90cebd885
Nick <9467802+Nachtalb@users.noreply.github.com>
2020-11-25 18:33:34 +0100

    Test multiple possible paths

--- init.fish
+++ init.fish
@@ -1,9 +1,17 @@
 # autojump initialization hook
 
-set -q autojump_fish; or set -g autojump_fish ~/.autojump/share/autojump/autojump.fish
-if test -f $autojump_fish
-  . $autojump_fish
+if set -q autojump_path and test -f $autojump_path
+  . $autojump_path
+else
+  for path in ~/.autojump/share/autojump/autojump.fish /usr/share/autojump/autojump.fish
+    if test -f $path
+      set -g autojump_fish $path
+      . $autojump_path
+      break
+    end
+  end
 end
+
 if not which autojump >/dev/null ^/dev/null
   echo 'Autojump is not installed. Please install it first from https://github.com/wting/autojump#installation' >&2
 end
